8c37c414d62213d3b76c6e8d9865c56f8f02a583
Tino <tinovalentin99@gmail.com>
2025-12-12 13:57:14 +0200

    viesti

--- mod02/tehtävä1.js
+++ mod02/tehtävä1.js
@@ -1,1 +1,9 @@
-console.log("Lorem...");
+const numbers = [];
+
+numbers[0] = window.prompt(`give a number: `);
+numbers[1] = window.prompt(`give a number: `);
+numbers[2] = window.prompt(`give a number: `);
+numbers[3] = window.prompt(`give a number: `);
+numbers[4] = window.prompt(`give a number: `);
+
+console.log(numbers);
