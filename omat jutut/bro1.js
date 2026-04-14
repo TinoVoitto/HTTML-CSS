834a2266ed401ea1819c473992096e1b204f5a22
Tino <tinovalentin99@gmail.com>
2026-04-14 13:47:01 +0300

    viesti

---
+++ omat jutut/bro1.js
@@ -0,0 +1,3 @@
+document.getElementById("message").textContent;
+document.getElementById("text").textContent;
+console.log(`hello from the console`);
--- omat jutut/teht.1.js
+++ omat jutut/teht.1.js
@@ -1,33 +1,7 @@
-console.log("I am printing to console");
-
 let username;
 
-document.getElementById("mySubmit").onclick = function () {
-  username = document.getElementById("myText").value;
+document.getElementById("submitButton").onclick = function () {
+  username = document.getElementById("inputName").value;
   console.log(username);
-  document.getElementById("myH3").textContent = `Hello, ${username}`;
-};
-
-let eka;
-
-document.getElementById("First").onclick = function () {
-  eka = document.getElementById("myFirst").value;
-  console.log(eka);
-  document.getElementById("Eka").textContent = `${eka}`;
-};
-
-let toka;
-
-document.getElementById("Second").onclick = function () {
-  toka = document.getElementById("mySecond").value;
-  console.log(toka);
-  document.getElementById("Toka").textContent = `${toka}`;
-};
-
-let kolme;
-
-document.getElementById("Third").onclick = function () {
-  kolme = document.getElementById("myThird").value;
-  console.log(kolme);
-  document.getElementById("Kolmas").textContent = `${kolme}`;
+  document.getElementById("greeting").textContent = `Hello, ${username}`;
 };
