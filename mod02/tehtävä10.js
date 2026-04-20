956b9d3742645b66e28c2946c2d974372600f006
Tino <tinovalentin99@gmail.com>
2026-04-20 15:18:44 +0300

    viesti

--- mod02/tehtävä10.js
+++ mod02/tehtävä10.js
@@ -1,5 +1,42 @@
-const minNum = 0;
-const maxNum = 10;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1));
+let NumOfCandidates = Number(window.prompt(`Enter the number of candidates: `));
+let candidates = [];
+let NumOfVotes = [];
 
-console.log(answer);
+for (let i = 1; i <= NumOfCandidates; i++) {
+  let name = window.prompt(`Enter the name of candidate ${i}:`);
+  candidates.push(name);
+}
+
+candidates.forEach((name) => {
+  console.log(`Candidate: ${name}
+Votes Received: 0`);
+});
+
+let Voters = Number(window.prompt(`Enter the total number of voters: `));
+let spreadVotes = Voters;
+
+for (let i = 0; i < candidates.length; i++) {
+  let amount = Math.floor(Math.random() * (spreadVotes + 1));
+  NumOfVotes.push(amount);
+  spreadVotes -= amount;
+}
+
+let emptyVotes = spreadVotes;
+
+let results = candidates.map((name, index) => {
+  return { name: name, votes: NumOfVotes[index] };
+});
+
+results.sort((a, b) => {
+  return b.votes - a.votes;
+});
+
+results.forEach((candidate) => {
+  console.log(`Candidate: ${candidate.name}
+Votes Received: ${candidate.votes}`);
+});
+
+let winner = results[0];
+
+console.log(`The winner is ${winner.name} with ${winner.votes} votes`);
+console.log(`Empty votes: ${emptyVotes}`);
---
+++ omat jutut/bro5.js
@@ -0,0 +1,47 @@
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+x = "0";
+y = "0";
+z = "0";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+x = "";
+y = "";
+z = "";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+let a;
+let b;
+let c;
+
+a = Number(a);
+b = String(b);
+c = Boolean(c);
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof c);
